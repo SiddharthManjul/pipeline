a49ee5deb130a48aa4d0e2d3214117bcbc7c7bcc
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-12-31 05:11:47 +0530

    homepage update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -50,7 +50,9 @@ export default function HomePage() {
 
       <Navbar onAuthModalOpen={handleAuthModalOpen} />
 
-      <HeroSection />
+      <div className="-mt-16">
+        <HeroSection />
+      </div>
 
       <div className="mt-24">
         <h3 className="text-4xl md:text-5xl font-bold text-center mb-12 text-muted-foreground">
